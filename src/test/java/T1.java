ddaf3d434da9e9f3a372baab3077133e342d9e61
hjd <hjd1622@qq.com>
2019-09-11 18:00:36 +0800

    对象（ObjectMapper 对象转化字符串）测试

--- src/test/java/T1.java
+++ src/test/java/T1.java
@@ -1,11 +1,41 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.xml.internal.ws.developer.SerializationFeature;
+import pojo.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2019/9/11.
  */
 public class T1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        // 如果json中有新增的字段并且是实体类类中不存在的，不报错
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
         RedisService redisService = new RedisService();
         redisService.set("hjd","789");
         System.out.println(redisService.get("hjd"));
+
+        List<String>  ssl = new ArrayList<String>();
+        ssl.add("12");
+        ssl.add("12");
+        ssl.add("12");
+        ssl.add("12");
+
+        String sd  = redisService.set("hjd1", mapper.writeValueAsString(ssl));
+        System.out.println(sd);
+        System.out.println(redisService.get("hjd1"));
+
+
+        User user = new User("hjd",25);
+        redisService.set("hjd2", mapper.writeValueAsString(user));
+        System.out.println(redisService.get("hjd2"));
+
+
 
     }
 }
